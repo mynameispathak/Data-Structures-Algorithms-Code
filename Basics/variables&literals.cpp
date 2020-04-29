eb063e99af926ac09d0539389df53046e724abcf
Aniket Pathak <171230012@nitdelhi.ac.in>
2020-04-29 18:31:12 +0530

    Create variables&literals.cpp

--- Basics/variables&literals.cpp
+++ Basics/variables&literals.cpp
@@ -18,4 +18,21 @@ int main()
     cout << h << endl;
     string i = "Hello";
     cout << i << endl;
+
+    //different ways of assignment
+    int j = 10;
+    cout << j << endl;
+    int k = 010;
+    cout << k << endl;
+    int l = 0x10;
+    cout << l << endl;
+    int m(10);
+    cout << m << endl;
+    int n = (10);
+    cout << n << endl;
+    int o{10};
+    cout << o << endl;
+    int p = {10};
+    cout << p << endl;
+    return 0;
 }
