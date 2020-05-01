202208f99bc0ab9f25c064fe9fb3c17ba96f2d4e
Aniket Pathak <171230012@nitdelhi.ac.in>
2020-05-02 02:26:11 +0530

    Update Power_Recursion.cpp

--- Recursion/Power_Recursion.cpp
+++ Recursion/Power_Recursion.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 using namespace std;
 
-int power(int x, int y)
+int power(int x, int y) // O(n)
 {
     if (y == 0)
         return 1;
@@ -11,10 +11,21 @@ int power(int x, int y)
         return x * power(x, y - 1);
 }
 
+int power1(int x, int y) // O(logn)
+{
+    if (y == 0)
+        return 1;
+    else if (y % 2 == 0)
+        return power1(x * x, y / 2);
+    else
+        return x * power(x * x, (y - 1) / 2);
+}
+
 int main()
 {
     int a, b;
     cout << "Enter two Numbers : ";
     cin >> a >> b;
-    cout << a << "^" << b << " = " << power(a, b);
+    cout << a << "^" << b << " = " << power(a, b) << endl;
+    cout << a << "^" << b << " = " << power1(a, b);
 }
