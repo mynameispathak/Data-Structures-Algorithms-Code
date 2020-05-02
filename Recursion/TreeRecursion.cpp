70d13e77d7b3db048540986cefde8c6f7b60675f
Aniket Pathak <171230012@nitdelhi.ac.in>
2020-05-03 01:15:49 +0530

    Create TreeRecursion.cpp

---
+++ Recursion/TreeRecursion.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int fun(int n)
+{
+    if (n > 0)
+    {
+        cout << n << " ";
+        fun(n - 1);
+        fun(n - 1);
+    }
+}
+
+int main()
+{
+    int a = 3;
+    fun(a);
+
+    return 0;
+}
